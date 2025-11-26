216ff276ca3e9a4687037a14c55488904fa024ec
Aldo Gordillo <a.gordillo@upm.es>
2025-11-26 16:48:51 +0100

    Minor changes

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -2,10 +2,8 @@ export const DEFAULT_APP_SETTINGS = {
   skin: "STANDARD",
   avatarImg: "/images/Profile_avatar_placeholder_large.png",
   userName: "User Name",
-  hint: "Enter your PIN to access the system",
-
   backgroundImg: "/images/background_screen.jpeg",
-  endScreenVideo: "/videos/windows_loading.mp4",
+  endScreenVideo: "/videos/loading.mp4",
 };
 
 export const THEMES = {
@@ -19,7 +17,7 @@ export const THEME_ASSETS = {
   [THEMES.RETRO]: {},
   [THEMES.STANDARD]: {
     backgroundImg: "/images/background_screen.jpeg",
-    endScreenVideo: "/videos/windows_loading.mp4",
+    endScreenVideo: "/videos/loading.mp4",
   },
   [THEMES.FUTURISTIC]: {},
 };
